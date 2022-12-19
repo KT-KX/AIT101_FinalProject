5bfe0a8a324475327522d9747a2f77af8ca2714d
Crax <55369708+KT-KX@users.noreply.github.com>
2022-12-19 21:43:47 +0800

    Add files via upload

--- main.c
+++ main.c
@@ -11,7 +11,6 @@ int infiniteloop=1;
 int mainmenuselection;
 
 struct CustomerDetails{
-
     char name[20];
     char phonenumber[15];
     char checkindate[15];
@@ -19,11 +18,12 @@ struct CustomerDetails{
     char roomnumber[10];
 }customer;
 
+void add();
 void edit();
 void removerecord();
 void view();
 void search();
-void add();
+
 void login(){
     char username[15] = "aitgroup10";
     char password[15] = "finalfullmark";
@@ -52,14 +52,9 @@ void login(){
             printf("\nTry again! or Press CTRL-C to terminate the program");
            }
         }
-        
-        
     }
-    
-        
-
-  
 }
+
 int main(void){
     time_t timenow;
     time(&timenow);
@@ -77,15 +72,13 @@ int main(void){
     printf("--------------------------------------------------------\n\n");
     printf("Press any key to login! ");
     getch();
-    //login();
-    while (validation==2)
-    {
+    login();
+    while (validation==2){
         exit(1);
     }
+
     system("cls");
- 
-    while (infiniteloop==1)
-    {   
+    while (infiniteloop==1){   
         printf("\n ---------------------------------------");
         printf("\n|     XMUM Hotel Reservation System     |");
         printf("\n|                                       |");
@@ -117,62 +110,11 @@ int main(void){
             printf("\n\nInvalid Input.Please Try Again");
             printf("\nPress any key to continue. ");
             getch();
-        }
-        
+        }    
     }
-    
     return 0;
-
 }
-
-
-void edit(){
-    FILE *fptr;
-    int j = 1;
-    char roomnumber[20];
-    long int size=sizeof(customer);
-    
-    if((fptr=fopen("C:\\Users\\USER\\Desktop\\10_FinalProject\\addroom.txt","r+"))==NULL);
-        printf("none");
-    
-    printf("\nEnter Your Room Number: ");
-    gets(roomnumber);
-    gets(roomnumber);
-    //printf("%s",roomnumber);
-    fflush(stdin);
-    while (fread(&customer,sizeof(customer),1,fptr)==1)
-    {
-        if(strcmp(customer.roomnumber,roomnumber)==0){
-            j=0;
-            printf("Please enter your name : ");
-            gets(customer.name);
-            printf("Please enter your phone number : ");
-            gets(customer.phonenumber);
-            printf("Please enter date to check-in (DD/MM/YYYY) : ");
-            gets(customer.checkindate);
-            printf("Please enter date to check-out (DD/MM/YYYY) : ");
-            gets(customer.checkoutdate);
-            printf("Please enter your Room number: ");
-            gets(customer.roomnumber);
-            printf("You have succesfully changed your details.\n");
-            printf("Thank you for choosing us.\n");
-            printf("\03\03\03");
-			fwrite(&customer,sizeof(customer),1,fptr);
-            fflush(stdin);
-			break;
-        }
-    }
-    if(j==1){
-		printf("\n\nTHE RECORD DOESN'T EXIST!!!!");
-		fclose(fptr);
-		getch();
-	}else{
-	fclose(fptr);
-	printf("\n\n\t\tYOUR RECORD IS SUCCESSFULLY EDITED!!!");
-    printf("\nPress any key to continue!");
-	getch();
-}
-}
+/*
 void removerecord(){
     system("cls");
     FILE *fptr;
@@ -194,8 +136,8 @@ void removerecord(){
       system("cls");
       printf("Enter your Room Number of the hotel to be removed: \n");
       scanf("%s",&roomnumber);
-      while(fread(&s,sizeof(s),1,fptr)==1){
-        if(strcmp(s.roomnumber,roomnumber)--0){
+      while(fread(&customer,sizeof(s),1,fptr)==1){
+        if(strcmp(s.roomnumber,roomnumber)==0){
           i = 0;
           continue;
         }
@@ -220,12 +162,15 @@ void removerecord(){
       close-1;
       break;
       case 'n':
-          close-1;
+          close=1;
           break;
       }
-      if(close--1)
+      if(close==1)
         break;
 }
+
+}*/
+
 void view(){
     system("cls");
     printf("please edit here");
@@ -236,25 +181,24 @@ void search(){
 }
 
 
+
 void add(){
     FILE *fptr;
-	fptr=fopen("C:\\Users\\USER\\Desktop\\10_FinalProject\\addroom.txt","a+");
-    if(fptr==NULL);
-    {
+	fptr=fopen("addroom.txt","a+");
+    if(fptr==NULL);{
         printf("Good day sir and madam!\n");
         printf("Please fill up the form below to reserve your room.\03\n");
+        printf("Please enter your Room number: ");
+        gets(customer.roomnumber);
+        gets(customer.roomnumber);
         printf("Please enter your name : ");
-        gets(customer.name);
         gets(customer.name);
         printf("Please enter your phone number : ");
         gets(customer.phonenumber);
         printf("Please enter date to check-in (DD/MM/YYYY) : ");
         gets(customer.checkindate);
         printf("Please enter date to check-out (DD/MM/YYYY) : ");
-        gets(customer.checkoutdate);
-        printf("Please enter your Room number: ");
-        gets(customer.roomnumber);
-
+        gets(customer.checkoutdate);  
         printf("You have succesfully booked a room.\n");
         printf("Thank you for choosing us.\n");
         printf("\03\03\03");
@@ -262,4 +206,53 @@ void add(){
     fwrite(&customer,sizeof(customer),1,fptr);
 	fflush(stdin);
     fclose(fptr);
-};
+    printf("\nPress any key to continue!");
+	getch();
+}
+
+void edit(){
+    FILE *fptr;
+    int j = 1;
+    char roomnumber[20];
+    long int size=sizeof(customer);
+    if((fptr=fopen("addroom.txt","r+"))==NULL)
+        exit(1);  
+    printf("\nEnter Your Room Number: ");
+    gets(roomnumber);
+    gets(roomnumber);
+   // printf("%s",roomnumber);
+    fflush(stdin);
+    while (fread(&customer,sizeof(customer),1,fptr)==1){
+        if(strcmp(customer.roomnumber,roomnumber)==0){
+            j=0;
+            printf("Please enter your Room number: ");
+            gets(customer.roomnumber);
+            printf("Please enter your name : ");
+            gets(customer.name);
+            fflush(stdin);
+            printf("Please enter your phone number : ");
+            gets(customer.phonenumber);
+            printf("Please enter date to check-in (DD/MM/YYYY) : ");
+            gets(customer.checkindate);
+            printf("Please enter date to check-out (DD/MM/YYYY) : ");
+            gets(customer.checkoutdate);
+            printf("You have succesfully changed your details.\n");
+            printf("Thank you for choosing us.\n");
+            printf("\03\03\03");
+            fseek(fptr,size,SEEK_CUR);
+			fwrite(&customer,sizeof(customer),1,fptr);
+			break;
+        }
+    }
+    if(j==1){
+		printf("\n\nTHE RECORD DOES NOT EXIST!!!!");
+		fclose(fptr);
+        printf("\nPress any key to continue!");
+		getch();
+	}else{
+	fclose(fptr);
+	printf("\n\nYOUR RECORD IS SUCCESSFULLY EDITED!!!");
+    printf("\nPress any key to continue!");
+	getch();
+    }   
+}
